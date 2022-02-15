bdd2f4a36801e21038d3e1e9b449f6eb5b10c16d
mehdicse <69783178+mahadi608@users.noreply.github.com>
2022-02-16 02:53:40 +0600

    first commit

---
+++ php/login.php
@@ -0,0 +1,78 @@
+<?php 
+require('config.php');
+extract($_REQUEST);
+if(isset($login))
+{
+  if($eid=="" || $pass=="")
+  {
+  $error= "<h4 style='color:red'>fill all details</h4>"; 
+  }   
+  else
+  {
+  $sql=mysqli_query($conn,"select * from login where email='$eid' && password='$pass' ");
+    if(mysqli_num_rows($sql))
+    {
+    $_SESSION['create_account_logged_in']=$eid;  
+    header('location:welcome.php'); 
+    }
+    else
+    {
+    $error= "<h4 style='color:red'>Invalid login details</h4>"; 
+    } 
+  }
+  
+}
+mysqli_close($conn);
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Online Hotel.Com</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="../css/login.css">
+  <link rel="stylesheet" href="../css/style.css">
+</head>
+<body>
+<div class="hero_in">
+      <div class="navbar">
+          <div class="icon">
+              <a href="#">
+                  <h2 class="logo">BlueStar</h2>
+              </a>
+          </div>
+          <div class="menu">
+              <ul>
+                  <li><a href="../index.html">HOME</a></li>
+                  <li><a href="#">ABOUT</a></li>
+                  <li><a href="#">SERVICE</a></li>
+                  <li><a href="#">CONTACT</a></li>
+                  <li><a href="login.html">
+                    <button class="btn">SIGNUP</button>
+                     </a>
+                  </li>
+              </ul>
+          </div>
+      </div>
+  <div class="container">
+    <div class="row">
+      <br>
+          <?php echo @$error; ?>
+          <form method="post" class="form">
+              <div class="form-group">
+                <input type="text" class="form-control" name="eid" placeholder="Email Id" autocomplete="off"required >
+              </div>
+            <div class="form-group">
+                <input type="password" class="form-control"name="pass"placeholder="Password" autocomplete="off"required>
+            </div>
+          <input type="submit" value="Login" name="login" class="btn-ln" required>
+          <div class="form-group forget">
+                <a href="Forgot_account.php">Forget Account</a>
+            </div>
+      	</form><br>
+        </div>
+    </div><br>
+  </div>
+</div>
+</body>
+</html>
---
+++ php/signup.php
@@ -0,0 +1,22 @@
+<?php
+include('connection.php');
+extract($_REQUEST);
+if(isset($save))
+{
+  $sql= mysqli_query($con,"select * from create_account where email='$email' ");
+  if(mysqli_num_rows($sql))
+  {
+  $msg= "<h1 style='color:red'> account already exists</h1>";    
+  }
+  else
+  {
+
+      $sql="insert into create_account(name,email,password,mobile,address,gender,country,pictrure) values('$fname','$email','$Passw','$mobi','$addr','$gend','$countr','$pict')";
+   if(mysqli_query($con,$sql))
+   {
+   $msg= "<h1 style='color:green'>Data Saved Successfully</h1>"; 
+   header('location:Login.php'); 
+   }
+  }
+}
+?>
