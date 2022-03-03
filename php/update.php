09992de424414f833b38a26f81881d72277d3ecc
mehdicse <69783178+mahadi608@users.noreply.github.com>
2022-03-03 11:43:54 +0600

    commit

--- php/update.php
+++ php/update.php
@@ -1,3 +1,14 @@
+<?php
+require "connection.php";
+$id=$_GET['id'];
+$name=$_GET['name'];
+$email=$_GET['email'];
+$mobile=$_GET['mobile'];
+$addr=$_GET['addr'];
+$gender=$_GET['gender'];
+$country=$_GET['count'];
+
+?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -5,7 +16,7 @@
     <title>Online Hotel.Com</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="../css/signup.css">
+    <!-- <link rel="stylesheet" href="../css/signup.css"> -->
     <!-- <link rel="stylesheet" href="../css/style.css"> -->
 </head>
 
@@ -16,22 +27,22 @@
                 <br>
                 <form method="post" class="form">
                     <div class="form-group">
-                        <input type="text" class="form-control" name="name" placeholder="name" autocomplete="off" required>
+                        <input type="text" class="form-control" name="name" placeholder="name" value="<?php echo "$name" ?>" required>
                     </div>
                     <div class="form-group">
-                        <input type="text" class="form-control" name="email" placeholder="Email" autocomplete="off" required>
+                        <input type="text" class="form-control" name="email" placeholder="Email" value="<?php echo "$email" ?>" required>
                     </div>
                     <div class="form-group">
-                        <input type="text" class="form-control" name="mobil" placeholder="mobile" autocomplete="off" required>
+                        <input type="text" class="form-control" name="mobil" placeholder="mobile" value="<?php echo "$mobile" ?>" required>
                     </div>
                     <div class="form-group">
-                        <input type="text" class="form-control" name="addr" placeholder="address" autocomplete="off" required>
+                        <input type="text" class="form-control" name="addr" placeholder="address" value="<?php echo "$addr" ?>" required>
                     </div>
                     <div class="form-group">
-                        <input type="text" class="form-control" name="contr" placeholder="country" autocomplete="off" required>
+                        <input type="text" class="form-control" name="contr" placeholder="country" value="<?php echo "$country" ?>" required>
                     </div>
                     <div class="form-group">
-                        <input type="text" class="form-control" name="gend" placeholder="gender" autocomplete="off" required>
+                        <input type="text" class="form-control" name="gend" placeholder="gender" value="<?php echo "$gender" ?>" required>
                     </div>
                     <input type="submit" value="UPDATE" name="login" class="btn-ln" required>
                 </form><br>
--- php/userdatashow.php
+++ php/userdatashow.php
@@ -1,22 +1,49 @@
 <!DOCTYPE html>
 <html>
+
 <head>
-<style>
-table, th, td {
-    border: 1px solid black;
-}
-</style>
+    <style>
+        table,
+        td,
+        th {
+            border: 1px solid;
+        }
+
+        table {
+            width: 100%;
+            border-collapse: collapse;
+            text-align: center;
+        }
+
+        button {
+            background-color: red;
+            width: 100px;
+            height: 30px;
+        }
+
+        .update {
+            background-color: green;
+            width: 100px;
+            height: 30px;
+        }
+
+        a {
+            text-decoration: none;
+            color: #fff;
+        }
+    </style>
 </head>
+
 <body>
 
-<?php
-require('connection.php');
+    <?php
+    require('connection.php');
 
-$sql = "select * from create_account ";
-$result = $conn->query($sql);
+    $sql = "select * from create_account ";
+    $result = $conn->query($sql);
 
-if ($result->num_rows > 0) {
-    echo "<table>
+    if ($result->num_rows > 0) {
+        echo "<table cellpadding=7 cellspacing=7>
     <tr>
     <th>ID</th>
     <th>Name</th>
@@ -25,28 +52,37 @@ if ($result->num_rows > 0) {
     <th>address</th>
     <th>gender</th>
     <th>country</th>
-    <th>operation</th>
+    <th colspan=2>operation</th>
     </tr>";
-    // output data of each row
-    while($row = $result->fetch_assoc()) {
-        echo "<tr>
-        <td>" . $row["id"]. "</td>
-        <td>" . $row["name"]. "</td>
-        <td>" . $row["email"]. "</td>
-        <td>" . $row["mobile"]. "</td>
-        <td>" . $row["address"]. "</td>
-        <td>" . $row["gender"]. "</td>
-        <td>" . $row["country"]. "</td>
-        <td><a href='delete.php?id=$row[id]'>DELETE  </a><a href='update.php?id=$row[id]'>UPDATE</a></td>
+        // output data of each row
+        while ($row = $result->fetch_assoc()) {
+            echo "<tr>
+        <td>" . $row["id"] . "</td>
+        <td>" . $row["name"] . "</td>
+        <td>" . $row["email"] . "</td>
+        <td>" . $row["mobile"] . "</td>
+        <td>" . $row["address"] . "</td>
+        <td>" . $row["gender"] . "</td>
+        <td>" . $row["country"] . "</td>
+        <td>
+        <button><a href='delete.php?id=$row[id]'>DELETE </a></button>
+        </td>
+        <td>
+        <button class=update>
+        <a href='update.php?id=$row[id] & name=$row[name] & email=$row[email] & mobile=$row[mobile] 
+        & addr=$row[address] & gender=$row[gender] & count=$row[country]'>UPDATE</a>
+        </button>
+        </td>
         </tr>";
+        }
+        echo "</table>";
+    } else {
+        echo "0 results";
     }
-    echo "</table>";
-} else {
-    echo "0 results";
-}
 
-$conn->close();
-?>
+    $conn->close();
+    ?>
 
 </body>
+
 </html>
