932d925d8212f4656625162c44f538a39a29a616
mehdicse <69783178+mahadi608@users.noreply.github.com>
2022-03-03 10:54:34 +0600

    commit

--- php/update.php
+++ php/update.php
@@ -1,19 +1,44 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
 
-require('connection.php');
+<head>
+    <title>Online Hotel.Com</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="../css/signup.css">
+    <!-- <link rel="stylesheet" href="../css/style.css"> -->
+</head>
 
-$id=$_GET['id'];
+<body>
+    <div class="hero_update">
+        <div class="container_update">
+            <div class="row">
+                <br>
+                <form method="post" class="form">
+                    <div class="form-group">
+                        <input type="text" class="form-control" name="name" placeholder="name" autocomplete="off" required>
+                    </div>
+                    <div class="form-group">
+                        <input type="text" class="form-control" name="email" placeholder="Email" autocomplete="off" required>
+                    </div>
+                    <div class="form-group">
+                        <input type="text" class="form-control" name="mobil" placeholder="mobile" autocomplete="off" required>
+                    </div>
+                    <div class="form-group">
+                        <input type="text" class="form-control" name="addr" placeholder="address" autocomplete="off" required>
+                    </div>
+                    <div class="form-group">
+                        <input type="text" class="form-control" name="contr" placeholder="country" autocomplete="off" required>
+                    </div>
+                    <div class="form-group">
+                        <input type="text" class="form-control" name="gend" placeholder="gender" autocomplete="off" required>
+                    </div>
+                    <input type="submit" value="UPDATE" name="login" class="btn-ln" required>
+                </form><br>
+            </div>
+        </div><br>
+    </div>
+    </div>
+</body>
 
-$sql="DELETE FROM create_account WHERE create_account.id= '$id'";
-$result = $conn->query($sql);
-
-if($result){
-    echo"Successfully UPDATE YOUR RECORD FROM DATABASE";
-}
-else{
-    echo"NOT UPDATE YOUR RECORD FROM DATABASE";
-}
-
-$conn->close();
-
-?>
+</html>
