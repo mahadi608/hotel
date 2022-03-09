47eca393f13bb429feeda834bcddb94e5181478a
mehdicse <69783178+mahadi608@users.noreply.github.com>
2022-03-09 16:13:19 +0600

    commit

--- admin/report.php
+++ admin/report.php
@@ -3,6 +3,22 @@
 
 <head>
     <link rel="stylesheet" href="../css/userdata.css">
+    <style>
+        .box {
+
+            width: 320px;
+            height: 200px;
+            padding: 10px;
+            background-color: aqua;
+            margin: 0;
+            border-radius: 10px;
+            text-align: center;
+
+        }
+        .head{
+            margin-top: 5%;
+        }
+    </style>
 </head>
 
 <body>
@@ -14,8 +30,11 @@
         </div>
         <div class="data">
             <h2> <u>ALL REPORT INFORMATION</u> </h2><br>
-
+            <div class="box"><h2 class="head">Total User</h2><h2>150</h2>
         </div>
+            
+        </div>
+       
     </div>
 
 </body>
