bd9f51c9d059db569fef81d76d9396f0a96426da
mehdicse <69783178+mahadi608@users.noreply.github.com>
2022-03-24 09:06:51 +0600

    commit

--- hotel.sql
+++ hotel.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3307
--- Generation Time: Mar 12, 2022 at 06:45 PM
+-- Generation Time: Mar 22, 2022 at 05:10 PM
 -- Server version: 10.4.22-MariaDB
 -- PHP Version: 8.1.2
 
@@ -62,25 +62,10 @@ CREATE TABLE `create_account` (
 --
 
 INSERT INTO `create_account` (`id`, `name`, `email`, `password`, `mobile`, `address`, `gender`, `country`) VALUES
-(4, 'Vishwaka', 'amitvish9999@gmail.com    ', '8190', 82015735, 'azamgarh    ', 'female', 'india'),
-(7, 'suraj vishwakarma', 'suraj@gmail.com   ', '8190', 56458521, 'kolpanday ', 'male', 'BD'),
-(9, 'Ragini Vishwakarm', 'ragini5@gmail.com     ', '1234`', 5684562, 'kolpanday(Kolgghat)     ', 'male', 'india'),
 (18, 'Bobby', 'BobbyEHorton@dayrep.com', '1234512', 810, '2407 Front Street', 'male', 'usa'),
 (19, 'Deborah', 'DeborahMSlagle@rhyta.com', '5648956', 501, '2309 Masonic Hill Road', 'female', 'American'),
-(20, 'Thomas', 'ThomasMPadgett@dayrep.com', '5642587', 512, '326 Short Street Austin', 'male', 'TX'),
-(21, 'Jonathan', 'JonathanFWatson@rhyta.com', '1564236', 7442892210, '2511 Bagwell Avenue Gainesville', 'male', 'UK');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `details_slider`
---
-
-CREATE TABLE `details_slider` (
-  `id` int(11) NOT NULL,
-  `img` varchar(255) NOT NULL,
-  `caption` varchar(50) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+(21, 'Jonathan', 'JonathanFWatson@rhyta.com', '1564236', 7442892210, '2511 Bagwell Avenue Gainesville', 'male', 'UK'),
+(22, 'shakil', 'shakil@gmail.com', '123456789', 165985632, 'dhaka', 'male', 'bangladesh');
 
 -- --------------------------------------------------------
 
@@ -101,7 +86,13 @@ CREATE TABLE `feedback` (
 --
 
 INSERT INTO `feedback` (`id`, `name`, `email`, `mobile`, `message`) VALUES
-(1, 'Amit', 'amitvish9999@gmail.com', 7275308190, 'Nice');
+(1, 'Amit', 'amitvish9999@gmail.com', 7275308190, 'Nice'),
+(2, 'shakil', 'shakil@gmail.com', 1682545452, 'good service '),
+(3, 'emran', 'emran@gmail.com', 156585245, 'bad service'),
+(4, 'emran', 'emran@gmail.com', 156585245, 'bad service'),
+(5, 'emran', 'emran@gmail.com', 156585245, 'bad service'),
+(6, 'emran', 'emran@gmail.com', 156585245, 'bad service'),
+(7, 'emran hasan', 'emran04@gmail.com', 1565852789, 'good');
 
 -- --------------------------------------------------------
 
@@ -122,7 +113,7 @@ CREATE TABLE `rooms` (
 --
 
 INSERT INTO `rooms` (`id`, `room_no`, `type`, `price`, `details`) VALUES
-(28, 512, 'Deluxe Room', 15000, 'The Contemporary yet simple designed King bedded rooms are well equipped with modern amenities. Fresh Decor and refined ambiance is the winning combination to make these rooms an ideal choice for discerning business traveler.'),
+(28, 512, 'Luxurious Suite  ', 18000, 'The Contemporary yet simple designed King bedded rooms are well equipped with modern amenities. Fresh Décor and refined ambiance is the winning combination to make these rooms an ideal choice for discerning business traveler.'),
 (30, 504, 'Luxurious Suite', 16000, 'Engulf yourself in the plush luxury of our premier rooms. An upgraded version of the Suite room, these rooms offer an elegant design with larger room space.'),
 (31, 302, 'Standard Room', 14000, 'Simple design king bedded room are well equipped with modern amenities.'),
 (32, 312, 'Suite', 13000, 'Enjoy the view of Anand from attach sitting area, An upgraded version of the Deluxe room, these rooms offer an elegant design with larger room space.'),
@@ -136,14 +127,7 @@ INSERT INTO `rooms` (`id`, `room_no`, `type`, `price`, `details`) VALUES
 
 CREATE TABLE `room_booking_details` (
   `id` int(11) NOT NULL,
-  `name` char(50) NOT NULL,
-  `email` varchar(50) NOT NULL,
-  `phone` bigint(20) NOT NULL,
-  `address` varchar(255) NOT NULL,
-  `city` varchar(100) NOT NULL,
-  `state` varchar(100) NOT NULL,
-  `zip` int(20) NOT NULL,
-  `contry` varchar(50) NOT NULL,
+  `Email` varchar(50) NOT NULL,
   `room_type` varchar(100) NOT NULL,
   `check_in_date` date NOT NULL,
   `check_in_time` varchar(6) NOT NULL,
@@ -155,33 +139,12 @@ CREATE TABLE `room_booking_details` (
 -- Dumping data for table `room_booking_details`
 --
 
-INSERT INTO `room_booking_details` (`id`, `name`, `email`, `phone`, `address`, `city`, `state`, `zip`, `contry`, `room_type`, `check_in_date`, `check_in_time`, `check_out_date`, `Occupancy`) VALUES
-(5, 'Sumit', 'sumit@gmail.com', 7398713060, 'kolpanday,azamgarh', 'Azamgarh', 'U.P.', 276001, 'India', 'Deluxe Room', '2019-12-05', '12:00', '2019-01-06', 'single'),
-(6, 'Om', 'om@gmail.com', 7890809, 'bnjkghjbjb', 'azamgarh', 'up', 276001, 'India', 'Deluxe Room', '2019-05-08', '08:00', '2019-06-04', 'single'),
-(7, 'Ragini Vishwakarma', 'ragini@gmail.com', 727550965, 'Kolpanday,Kolghat,Azamgarh', 'Azamgarh', 'U.P', 276001, 'India', 'Standard Room', '2019-12-06', '08:00', '2019-12-06', 'single'),
-(8, 'Anlaji viahwakarma', 'anjali@gmail.com', 7275308190, 'kolpanday azamgarh', 'azamgarh', 'U.P', 276001, 'India', 'Standard Room', '2019-12-06', '08:00', '2019-12-06', 'single'),
-(12, 'sanjeev', 'sanjeevtech2@gmail.com', 0, 'dfjdlfj', '', '', 0, '', 'Suite Room', '2019-05-22', '22:57', '2017-10-31', 'single');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `slider`
---
-
-CREATE TABLE `slider` (
-  `id` int(11) NOT NULL,
-  `image` varchar(100) NOT NULL,
-  `caption` varchar(100) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `slider`
---
-
-INSERT INTO `slider` (`id`, `image`, `caption`) VALUES
-(3, 'img3.jpg', ''),
-(6, 'img2.jpg', ''),
-(8, 'img1.png', '');
+INSERT INTO `room_booking_details` (`id`, `Email`, `room_type`, `check_in_date`, `check_in_time`, `check_out_date`, `Occupancy`) VALUES
+(5, 'amitvish9999@gmail.com', 'Deluxe Room', '2019-12-05', '12:00', '2019-01-06', 'single'),
+(6, 'ragini5@gmail.com', 'Deluxe Room', '2019-05-08', '08:00', '2019-06-04', 'single'),
+(8, 'DeborahMSlagle@rhyta.com', 'Standard Room', '2019-12-06', '08:00', '2019-12-06', 'single'),
+(13, 'shakil04@gmail.com   ', 'Room   ', '2022-03-26', '12:00', '2022-03-28', 'single'),
+(20, 'shakil@gmail.com', 'Standard Room', '2022-03-28', '01:01', '2022-03-31', 'dubble');
 
 --
 -- Indexes for dumped tables
@@ -197,12 +160,6 @@ ALTER TABLE `admin`
 -- Indexes for table `create_account`
 --
 ALTER TABLE `create_account`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `details_slider`
---
-ALTER TABLE `details_slider`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -224,12 +181,6 @@ ALTER TABLE `room_booking_details`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `slider`
---
-ALTER TABLE `slider`
-  ADD PRIMARY KEY (`id`);
-
---
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -243,19 +194,13 @@ ALTER TABLE `admin`
 -- AUTO_INCREMENT for table `create_account`
 --
 ALTER TABLE `create_account`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
-
---
--- AUTO_INCREMENT for table `details_slider`
---
-ALTER TABLE `details_slider`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
 
 --
 -- AUTO_INCREMENT for table `feedback`
 --
 ALTER TABLE `feedback`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT for table `rooms`
@@ -267,13 +212,7 @@ ALTER TABLE `rooms`
 -- AUTO_INCREMENT for table `room_booking_details`
 --
 ALTER TABLE `room_booking_details`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
-
---
--- AUTO_INCREMENT for table `slider`
---
-ALTER TABLE `slider`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
